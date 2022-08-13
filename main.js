69a6b221df42269a7bc61f4a53c32d677950f686
betoaleman1 <105089286+betoaleman1@users.noreply.github.com>
2022-08-12 19:57:54 -0500

    Add files via upload
    WARNING
    
    this app uses camera recognition so think about it before tring it 
    :)

--- main.js
+++ main.js
@@ -20,3 +20,17 @@ console.log('ml5 version',ml5.version);
 
 classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/lLKemjp9z/model.json', modelLoaded);
 function modelLoaded(){console.log("modelo cargado")}
+
+function check() {
+     img = document.getElementById('captured_image');
+      classifier.classify(img, gotResult); 
+    }
+
+    function gotResult(error, results) {
+         // Muestra el error en la consola 
+         if (error) { console.error(error); } else { 
+            // Los resultados están en un array ordenado por presición. 
+            console.log(results);
+             document.getElementById("result_object_name").innerHTML = results[0].label;
+              document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+             } }
